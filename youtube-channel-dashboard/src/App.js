9c3f3187c3979c53a7082d0594b4ff12d15a1822
Eunoia-12 <117347614+Eunoia-12@users.noreply.github.com>
2024-04-30 18:09:29 -0400

    Add files via upload

---
+++ youtube-channel-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div> 
+      <div class = "main-title">YouTube Channel Dashboard</div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
